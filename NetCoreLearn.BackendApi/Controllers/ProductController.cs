d7ec41eb7e7accb6c5b3a73d20ca2607ca640d5c
ToanNgo2709 <ngotoanlibra@gmail.com>
2021-07-05 20:38:48 +0700

    fix foo

--- NetCoreLearn.BackendApi/Controllers/ProductController.cs
+++ NetCoreLearn.BackendApi/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NetCoreLearn.BLL.Catalog.Products;
+using NetCoreLearn.ViewModel.Catalog.Products.Dtos.Manage;
+using NetCoreLearn.ViewModel.Catalog.Products.Dtos.Public;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +15,12 @@ namespace NetCoreLearn.BackendApi.Controllers
     public class ProductController : ControllerBase
     {
         private readonly IPublicProductService _publicProductService;
+        private readonly IManageProductService _manageProductService;
 
-        public ProductController(IPublicProductService publicProductService)
+        public ProductController(IPublicProductService publicProductService, IManageProductService manageProductService)
         {
             this._publicProductService = publicProductService;
+            this._manageProductService = manageProductService;
         }
 
         [HttpGet]
@@ -25,5 +29,30 @@ namespace NetCoreLearn.BackendApi.Controllers
             var products = await _publicProductService.GetAll();
             return Ok(products);
         }
+
+        [HttpGet("{public-paging}")]
+        public async Task<IActionResult> Get([FromQuery] GetPublicProductPagingRequest request)
+        {
+            var products = await _publicProductService.GetAllByCategoryId(request);
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromQuery] int id)
+        {
+            var products = await
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]ProductCreateRequest request)
+        {
+            var products = await _manageProductService.CreateProduct(request);
+            if (products == 0)
+            {
+                return BadRequest();
+            }
+            return Created();
+        }
+
     }
 }
